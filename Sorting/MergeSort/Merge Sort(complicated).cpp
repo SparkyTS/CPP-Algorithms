8f3c6d3034c7f02aee43c2c0ad87c82d575beff5
Tanay <48762037+SparkyTS@users.noreply.github.com>
2019-03-24 18:44:10 +0530

    Merge Sort
    Merge sort (simple) is implemented in very common way 
    Merges sort(complicated) is implemented when I don't know the simple algorithm to implement it

---
+++ Sorting/MergeSort/Merge Sort(complicated).cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int* get_random_data(int size)//Generate random array of integer
+{
+    int* data = new int[size];
+    /*
+    ///For sorting char type of data
+    Replace int*      with   char*
+            new int   with   new char  && comment line 23 to 25
+    data[0]='t';
+    data[1]='a';
+    data[2]='n';
+    data[3]='a';
+    data[4]='y';
+    data[5]='_';
+    data[6]='s';
+    data[7]='h';
+    data[8]='a';
+    data[9]='h';
+*/
+    srand(time(NULL));
+        for(int i=0;i<size;i++)
+        data[i]=100*rand()/RAND_MAX;
+   return data;
+}
+print_array(int* array , int size )
+{
+    for(int i=0;i<size;i++)
+        cout<<array[i]<<" ";
+}
+merge(int* array1,int n1,int index1,int* array2,int n2,int index2,int* sorted,int index){
+    while(n1 && n2)
+    {
+        if(array1[index1]<array2[index2]){
+            sorted[index]=array1[index1];
+            index++,index1++,n1--;
+        }
+        else{
+            sorted[index]=array2[index2];
+            index++,index2++,n2--;
+        }
+    }
+    while(n1){
+        sorted[index]=array1[index1];
+        index++,index1++;n1--;
+    }
+    while(n2){
+        sorted[index]=array2[index2];
+        index++,index2++,n2--;
+    }
+}
+mergepass(int* const array,int size,int C,int* const sorted)
+{                        //C=current_no_elements
+    int P = size/(2*C);  //no of pair
+    int m_size = 2*C*P;  //size of array after merging two arrays
+    int R = size-m_size; //Remaining Array to merge
+    int LB;              //Lower Bound - Index of first element in array
+    for(int j=0;j<P;j++){
+        LB=2*j*C;
+        merge(array,C,LB,array,C,LB+C,sorted,LB);
+    }
+    if(R<=C)           //IF only one sub array left
+    {
+        for(int j=0;j<R;j++)
+            sorted[m_size+j]=array[m_size+j];//Copying data as it is
+    }
+    else
+    {
+        merge(array,C,m_size,array,R-C,m_size+C,sorted,m_size);
+    }
+}
+merge_sort(int* array,int size)
+{
+    int C=1;int* sorted=new int[size];
+    while(C<size)
+    {
+     mergepass(array,size,C,sorted);
+     mergepass(sorted,size,2*C,array);
+     C=4*C;
+    }
+}
+main()
+{
+int* data,size=10;
+data=get_random_data(size);
+cout<<"Unsorted Data : ";
+print_array(data,size);
+merge_sort(data,size);
+cout<<"\n  Sorted Data : ";
+print_array(data,size);
+}
---
+++ Sorting/MergeSort/Merge sort (simple).cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int* get_random_data(int size)//Generate random array of integer
+{
+    int* data = new int[size];
+
+    srand(time(NULL));
+        for(int i=0;i<size;i++)
+        data[i]=100*rand()/RAND_MAX;
+   return data;
+}
+print_array(int* array , int size )
+{
+    for(int i=0;i<size;i++)
+        cout<<array[i]<<" ";
+}
+
+merge(int* array,int left , int mid , int right)
+{
+int i,j,k;
+int n1=mid-left+1;
+int n2=right-mid;
+int l_array[n1] , r_array[n2];
+
+for(i=0 ; i<n1 ; i++)
+    l_array[i] = array[left + i];
+for(j=0 ; j<n2 ; j++)
+    r_array[j] = array[mid  + j + 1];
+
+i=0;j=0;k=left;
+
+    while( n1 && n2)
+    {
+        if(l_array[i] < r_array[j]){
+            array[k] = l_array[i];
+            i++,k++,n1--;
+        }
+        else{
+            array[k] = r_array[j];
+            k++,j++,n2--;
+        }
+    }
+
+    while(n1){
+      array[k] = l_array[i];
+       k++,i++,n1--;
+    }
+
+
+    while(n2){
+        array[k] = r_array[j];
+        k++,j++,n2--;
+    }
+}
+
+merge_sort(int* array,int left,int right)
+{
+    if(left<right)
+    {
+     int mid = (left+right)/*(right - left)*/ / 2;
+     merge_sort(array , left  , mid);
+     merge_sort(array , mid+1 , right);
+
+     merge(array,left,mid,right);
+    }
+}
+
+main()
+{
+int* data,size=10;
+data=get_random_data(size);
+cout<<"Unsorted Data : ";
+
+print_array(data,size);
+
+merge_sort(data,0,size-1);
+
+cout<<"\n  Sorted Data : ";
+print_array(data,size);
+}
